8ab3a5ca33a40fc289b1991087bb89d92a73e3eb
Saswat Pal <saswat2001@gmail.com>
2026-05-06 10:48:43 +0530

    edit readme

--- url-shortener-react/src/components/NavBar.jsx
+++ url-shortener-react/src/components/NavBar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         <ul
           className={`flex sm:gap-10 gap-4 sm:items-center sm:mt-1 sm:pt-0 pt-3 text-slate-800 sm:static absolute left-0 top-[62px] sm:shadow-none shadow-md ${
             navbarOpen ? "h-fit sm:pb-0 pb-5" : "h-0 overflow-hidden"
-          }  transition-all duration-100 sm:h-fit sm:bg-none sm:w-fit w-full sm:flex-row flex-col px-4 sm:px-0`}
+          }  transition-all duration-100 sm:h-fit sm:bg-none bg-custom-gradient sm:w-fit w-full sm:flex-row flex-col px-4 sm:px-0`}
         >
           <li className="hover:text-btnColor font-[500]  transition-all duration-150">
             <Link
